9e683d2c81b319cf1eff6629742d20ccac9e2456
Sourabh Soni <sourabh86@gmail.com>
2020-08-07 22:08:57 +0530

    Updated siteConfig

--- siteConfig.js
+++ siteConfig.js
@@ -1,22 +1,22 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://demo.jamify.org`,
+    siteUrl: `https://sourabhsoni.com`,
 
     // Enable infinite scroll (default: true)
     infiniteScroll: true,
 
     // Initial number fetched, scrolling lazy loads posts one by one
     // If infinite scroll is disabled: maximum number of post shown per page
-    postsPerPage: 3,
+    postsPerPage: 5,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `Life and learnings of a codesmith`,
 
     // This allows an site description for meta data for pages.
-    siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
+    siteDescriptionMeta: ``,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
+    shortTitle: `SourabhSoni`,
     siteIcon: `favicon.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
